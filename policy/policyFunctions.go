c6dc4c7e7abaa4291e920b4905330bc25f139f65
Lars Blütecher Holter <larsbho@stud.ntnu.no>
2022-05-03 08:37:08 +0200

    Fixed stringency so it matches the assignment

--- policy/policyFunctions.go
+++ policy/policyFunctions.go
@@ -16,16 +16,20 @@ func storePolicyData(data map[string]interface{}) consts.PolicyResults {
 			-1,
 			-1,
 			-1,
-			-1,
 		}
 	} else {
+		// Check if the 'stringency_actual' field is nil, if it is: use the 'stringency' field instead.
+		stringency := data["stringency_actual"].(float64)
+		if data["stringency_actual"] == nil {
+			stringency = data["stringency"].(float64)
+		}
+
 		return consts.PolicyResults{
 			data["date_value"].(string),
 			data["country_code"].(string),
 			data["confirmed"].(float64),
 			data["deaths"].(float64),
-			data["stringency_actual"].(float64),
-			data["stringency"].(float64),
+			stringency,
 			0,
 		}
 	}
--- variables/structs.go
+++ variables/structs.go
@@ -16,13 +16,12 @@ type Results struct {
  *	Struct for the polict results we got in the policies
  */
 type PolicyResults struct {
-	Date             string  `json:"date_value"`
-	CountryCode      string  `json:"country_code"`
-	Confirmed        float64 `json:"confirmed"`
-	Deaths           float64 `json:"deaths"`
-	StringencyActual float64 `json:"stringency_actual"`
-	Stringency       float64 `json:"stringency"`
-	Policy           int     `json:"policy"`
+	Date        string  `json:"date_value"`
+	CountryCode string  `json:"country_code"`
+	Confirmed   float64 `json:"confirmed"`
+	Deaths      float64 `json:"deaths"`
+	Stringency  float64 `json:"stringency"`
+	Policy      int     `json:"policy"`
 }
 
 /**
